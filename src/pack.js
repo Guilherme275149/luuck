6bf7a4ea4a6d9162c90fe5782da7004bcd5b2401
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-04-15 15:04:57 -0300

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,33 @@
+const pack = (date, user, wame) => {
+return `┏━𝙀𝙡𝙞𝙯𝙖𝙗𝙚𝙩𝙝 𝘽𝙊𝙏 ━┓
+║                                                           
+║ 𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║ 𝙒𝘼𝙈𝙀 : ${wame}                                        
+║                                                          
+║
+┗═════════════┛
+» 🥵𝙾𝚗𝚕𝚢𝚏𝚊𝚗𝚜 𝚎 𝚑𝚎𝚗𝚝𝚊𝚒 𝚗𝚘 𝚖𝚎𝚐𝚊🥵 «
+
+❁❧ *FOTOS DE HENTAI 720MB:* 
+_*🌐 https://suaurl.com/55eb2f 🌐*_
+
+❁❧ *PRINCESS STARLING(ONLY FANS)8.36GB:* 
+_*🌐 https://suaurl.com/b995e2 🌐*_
+
+❁❧ *ELLES CLUB 28.68GB:* 
+_*🌐 https://suaurl.com/8c135b 🌐*_
+
+❁❧ *COCONUTKITTY(ONLY FANS)7.69GB:*
+_*🌐 https://suaurl.com/5e91e0 🌐*_
+
+❁❧ *Belle delphine:* 
+_*🌐 https://suaurl.com/5c699d 🌐*_
+
+❁❧ *Love Lilah:* 
+_*🌐 https://suaurl.com/dbaae7 🌐*_
+
+❁❧ *Hidori:* 
+_*🌐 https://suaurl.com/f43f76 🌐*_`
+}
+exports.pack = pack
